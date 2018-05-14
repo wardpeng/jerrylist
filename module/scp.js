c622f3eb7992c66d691d6b23706500cea14f09b1
i042416 <cle.ee@163.com>
2018-05-14 10:26:43 +0800

    add to free discussion

--- module/scp.js
+++ module/scp.js
@@ -4,7 +4,7 @@ var document_service = function(value){
 　　console.log("SCP document service: " + value);
 }
 
-// //不能写成export sex这样的方式，如果这样就相当于export "boy",
+// 不能写成export sex这样的方式，如果这样就相当于export "boy",
 // 外部文件就获取不到该文件的内部变量sex的值，因为没有对外输出变量接口,只是输出的字符串。
 export {scp,document_service}  
 
